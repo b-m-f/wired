38a39b11adcda5dae1bc78244b229e19ad9a58a7
Maximilian Ehlers <maximilian@ehlers.berlin>
2023-08-17 16:24:16 +0200

    Adds nix flake for dev dependencies
    Signed-off-by: Maximilian Ehlers <maximilian@ehlers.berlin>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nix flake for wired";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (self: super: rec {
+        })
+      ];
+      pkgs = import nixpkgs { inherit overlays system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ podman buildah bash];
+
+        shellHook = ''
+          echo "Ready to wire"
+        '';
+      };
+    });
+}
