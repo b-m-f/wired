7c8bc56d1f513bee323c2bc44d103d35e6c31bc2
Maximilian Ehlers <maximilian@sodawa.com>
2024-08-12 17:33:51 +0200

    adds cram tests
    Signed-off-by: Maximilian Ehlers <maximilian@sodawa.com>

--- flake.nix
+++ flake.nix
@@ -2,31 +2,36 @@
   description = "Nix flake for wired";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
+    {
+      self,
+      nixpkgs,
+      flake-utils,
     }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [
-        (self: super: rec {
-        })
-      ];
-      pkgs = import nixpkgs { inherit overlays system; };
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ podman buildah bash];
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (self: super: rec { }) ];
+        pkgs = import nixpkgs { inherit overlays system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            podman
+            buildah
+            bash
+            python312Packages.cram
+          ];
 
-        shellHook = ''
-          echo "Ready to wire"
-        '';
-      };
-    });
+          shellHook = ''
+            echo "Ready to wire"
+          '';
+        };
+      }
+    );
 }
---
+++ tests/cram/interface.t
@@ -0,0 +1,4 @@
+Print missing config when invoked without anything
+  $ wired
+  Please specify config file to be used. Use -help for command line interface description
+  [1]
